73ec5a517b870c4b3f5ad37384c6aa5cab767093
Lucas Schmidt <lucas.schmidt015@gmail.com>
2025-03-15 21:51:10 -0300

    feat: new initial configs such errorMiddleware and new routes and controllers.

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,14 +1,29 @@
-import express, { Request, Response } from "express";
+import express, { Request } from "express";
+import * as dotenv from 'dotenv';
+const cors = require('cors');
+import path from 'path';
+
+import erroHandler from "./middlewares/erroHandler";
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+const PORT = parseInt(process.env.API_PORT || '3000', 10);
+const HOST = process.env.API_HOST || 'localhost';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+app.use(cors({ 
+  origin: "*",
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
-});
+//Here we can define our routes
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.use(erroHandler);
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
 });
