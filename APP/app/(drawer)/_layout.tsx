d7e268a7d707e2ead99cb4b164e80d14c9a83cc5
Lucas Schmidt <lucas.schmidt015@gmail.com>
2025-05-01 11:28:08 -0300

    feat: Profile infos added to the drawer.

--- APP/app/(drawer)/_layout.tsx
+++ APP/app/(drawer)/_layout.tsx
@@ -1,18 +1,79 @@
 import { Drawer } from 'expo-router/drawer';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-export default function DrawerLayout() {
+import { View, Text, Image, StyleSheet, useColorScheme } from 'react-native';
+import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import ProfilePicture from '@/assets/images/profile.png';
+import EmptyProfile from '@/assets/images/EmptyProfile.jpeg';
+
+function CustomDrawerContent(props: any) {
+  const colorScheme = useColorScheme();
+  const themeStyles = {
+    textColor: colorScheme === 'dark' ? '#fff' : '#000',
+    subTextColor: colorScheme === 'dark' ? '#aaa' : '#666',
+    borderColor: colorScheme === 'dark' ? '#555' : '#ccc',
+  };
 
   return (
+    <DrawerContentScrollView {...props}>
+      <View style={[styles.profileContainer, { borderBottomColor: themeStyles.borderColor }]}>
+        <Image
+          source={ProfilePicture}
+          style={styles.profileImage}
+        />
+        <View style={styles.infoContainer}>
+          <Text style={[styles.profileName, { color: themeStyles.textColor }]}>Lucas Torchelsen Schmidt</Text>
+          <Text style={[styles.profileEmail, { color: themeStyles.subTextColor }]}>lucas.schmidt015@gmail.com</Text>
+        </View>
+      </View>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
+export default function DrawerLayout() {
+  return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
+      <Drawer
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
         <Drawer.Screen
-            name="(home)"
-            options={{
-              drawerLabel: 'Home',
-              headerShown: false,
-            }}
+          name="(home)"
+          options={{
+            drawerLabel: 'Home',
+            headerShown: false,
+          }}
         />
       </Drawer>
     </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    padding: 20,
+    // alignItems: 'center',
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: 10,
+  },
+  profileImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginBottom: 10,
+  },
+  infoContainer: {
+    marginLeft: 15,
+    justifyContent: 'flex-end',
+    paddingBottom: 15
+  },
+  profileName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  profileEmail: {
+    fontSize: 14,
+    color: '#666',
+  },
+});
