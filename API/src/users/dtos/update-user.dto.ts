9014e1f98059c64eaf1a02856c407c0bebc30674
Lucas Schmidt <lucas.schmidt015@gmail.com>
2025-05-10 10:19:45 -0300

    feat: adding the application's first modules.

---
+++ API/src/users/dtos/update-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsString, MinLength, IsDateString } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @MinLength(2)
+  name: string;
+
+  @IsDateString()
+  birthdayDate: string;
+}
