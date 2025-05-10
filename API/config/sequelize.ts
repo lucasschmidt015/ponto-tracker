0782f254796b7deff5b446a91efc237feb977c6d
lucasschmidt015 <lucas.schmidt015@gmail.com>
2025-05-10 19:38:45 -0300

    feat: setting up script to create the database through Sequelize CLI

--- API/config/sequelize.ts
+++ API/config/sequelize.ts
@@ -1,14 +1,14 @@
-// import configs from 'config';
+import configs from 'config';
 
-// const config = {
-//   database: db.database,
-//   username: db.username,
-//   password: db.password,
-//   host: db.options.host,
-//   port: db.options.port,
-//   dialect: db.options.dialect,
-//   migrationStorage: 'sequelize',
-//   seederStorage: 'sequelize',
-//   seederStorageTableName: '_sequelize_seeders',
-//   migrationStorageTableName: '_sequelize_migrations',
-// };
+const { db } = configs;
+
+const config = {
+  database: db.database,
+  username: db.username,
+  password: db.password,
+  host: db.host,
+  port: db.port,
+  dialect: db.dialect,
+};
+
+export = config;
