e3adb253a6c67765c22e3400aeb7ca16571c5fc6
lucasschmidt015 <lucas.schmidt015@gmail.com>
2025-06-02 21:14:09 -0300

    feat: Adding default data through seeders

--- API/config/sequelize.ts
+++ API/config/sequelize.ts
@@ -1,14 +1,14 @@
-import configs from 'config';
+import configs from './';
 
 const { db } = configs;
 
 const config = {
-  database: db.database,
-  username: db.username,
-  password: db.password,
-  host: db.host,
-  port: db.port,
-  dialect: db.dialect,
+	database: db.database,
+	username: db.username,
+	password: db.password,
+	host: db.host,
+	port: db.port,
+	dialect: db.dialect,
 };
 
 export = config;
