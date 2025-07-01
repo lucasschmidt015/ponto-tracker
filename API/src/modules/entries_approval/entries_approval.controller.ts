0279e8f418ee95047bbacb1dffc39b97edddcfa8
lucasschmidt015 <lucas.schmidt015@gmail.com>
2025-07-01 20:45:16 -0300

    Adds entry approval endpoint
    Introduces a new endpoint to handle entry approval requests.
    
    This endpoint allows users to submit approval requests, which are then processed by the associated service.

--- API/src/modules/entries_approval/entries_approval.controller.ts
+++ API/src/modules/entries_approval/entries_approval.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { EntriesApprovalService } from './entries_approval.service';
+
+import { ApproveEntryApprovalDto } from './dtos/approve_entry_approval_dto';
 
 @Controller('entries-approval')
-export class EntriesApprovalController {}
+export class EntriesApprovalController {
+	constructor(private entriesApprovalService: EntriesApprovalService) {}
+
+	@Post()
+	approvalEntryApproval(@Body() entryApproval: ApproveEntryApprovalDto) {
+		return this.entriesApprovalService.approveEntryApproval(entryApproval);
+	}
+}
